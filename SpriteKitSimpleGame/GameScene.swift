072619084dcfaee113ebf458e367ed9331a3ff31
Podge Hession <C13329991@mydit.ie>
2014-12-21 14:14:09 +0000

    Second Commit

--- SpriteKitSimpleGame/GameScene.swift
+++ SpriteKitSimpleGame/GameScene.swift
@@ -7,39 +7,56 @@
 //
 
 import SpriteKit
-
 class GameScene: SKScene {
+    //create player sprite 
+    // 1 -- declare a private constant for the player (i.e. the ninja) which is an eg of a sprite
+    let player = SKSpriteNode(imageNamed: "player")
     override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
-        
-        self.addChild(myLabel)
+    // 2 -- set background color
+    backgroundColor = SKColor.whiteColor()
+    // 3 -- Position the sprite to be 10% across vertically and centered horizontally
+    player.position = CGPoint(x: size.width * 0.1, y: size.height * 0.5)
+    // 4 -- to make the sprite appear on the screen, you just add it as a child of the scene (this is similar to how you make views children of other views)
+    addChild(player)
+    
+    
+    //call the addMonster function to create monsters
+    runAction(SKAction.repeatActionForever(
+        SKAction.sequence([
+        SKAction.runBlock(addMonster),
+        SKAction.waitForDuration(1.0)
+        ])
+    ))
     }
     
-    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
-        /* Called when a touch begins */
-        
-        for touch: AnyObject in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
+    
+    
+    //Monster Enemy methods
+    func random() -> CGFloat {
+        return CGFloat(Float(arc4random()) / 0xFFFFFFFF)
     }
-   
-    override func update(currentTime: CFTimeInterval) {
-        /* Called before each frame is rendered */
+    func random(#min: CGFloat, max: CGFloat) -> CGFloat {
+        return random() * (max - min) + min
+    }
+    func addMonster() {
+        // Create sprite
+        let monster = SKSpriteNode(imageNamed: "monster")
+        // Determine where to spawn the monster along the Y axis
+        let actualY = random(min: monster.size.height/2, max: size.height -
+        monster.size.height/2)
+        // Position the monster slightly off-screen along the right edge,
+        // and along a random position along the Y axis as calculated above 
+        monster.position = CGPoint(x: size.width + monster.size.width/2, y: actualY)
+       // Add the monster to the scene
+        addChild(monster)
+        
+        //actions
+        // Determine speed of the monster
+        let actualDuration = random(min: CGFloat(2.0), max: CGFloat(4.0))
+        // Create the actions
+        let actionMove = SKAction.moveTo(CGPoint(x: -monster.size.width/2, y: actualY),
+        duration: NSTimeInterval(actualDuration))
+        let actionMoveDone = SKAction.removeFromParent()
+        monster.runAction(SKAction.sequence([actionMove, actionMoveDone]))
     }
 }
